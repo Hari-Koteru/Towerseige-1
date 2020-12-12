a55e671a7db655db2b14ebb27c959a3614ec2d7c
Hari-Koteru <73772978+Hari-Koteru@users.noreply.github.com>
2020-12-12 14:40:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var ground,background;
+var polygon,polygonImage;
+
+function preload(){
+  polygonImage = loadImage("Polygon.png");
+}
+
+function setup() {
+  createCanvas(1000,700);
+  ground = createSprite(200,100,20,10);  
+  ground.shapeColor = "green";
+}
+
+function draw(){
+background(0);
+
+
+
+
+}
+
+
